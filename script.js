9cfb316533418cc7115da6d927301792e920ea94
Jessica Perry <l33thedgieperry@gmail.com>
2023-06-10 00:54:33 -0700

    Project 1st draft
    This change meets fuctionality requires for project.
    - form with min of five questions
    - output min result of 3 languages
    - Branching/conditional statement
    - form creates diverse answer per submission
    - My code handles event handler properties and eventlisteners
    - Variable namnes are descriptive and use lower camel case

--- script.js
+++ script.js
@@ -1,37 +1,14 @@
-// function likeEggs() {
-//   let likeEggs = prompt("How many eggs do you eat in a day?");
-//   let p = document.querySelector("p");
-//   if (likeEggs >= 5) {
-//     p.innerHTML = "😍 <span><p>I love eggs! </p></span>";
-//   } else {
-//     p.innerHTML = "😏 <span><p>Eggs are amazing! </p></span>";
-//   }
-// }
-// let likeEggsButton = document.querySelector("button");
-// likeEggsButton.addEventListener("click", likeEggs);
+//question one
+function checkButton() {
+  var getSelectedValue = document.querySelector(
+    'input[name="taste"]:checked'
+  ).value;
 
-// ~The above code is a function to change the header tag ( which is a p tag) into
-//~ different title changes
-
-// const python = ["extrovert", "salty", "anywhere","hedgehog"];
-// const javascript = ["introvert", "sweet", "firm", "cat"];
-// const golang = ["ambivert", "both", "soft", "dog"];
-
-//using one question from your html page, create a javascript function that
-//will display ONE programming language at the bottom of the page after I click
-//submit
-
-//querySelector() method to access the matched element.
-
-document.getElementById("extrovert").onclick = function () {
-  let selected = document.querySelector(
-    "input[type=radio] [name=personality]:checked"
-  );
-  alert(selected.value);
-};
-//below possible way to display in bottom of page
-function questionOne() {
-  let questionOne = document.getElementById("extrovert");
-  let display = document.querySelector("h3");
+  if (getSelectedValue == "salty") {
+    document.getElementById("disp").innerHTML = "Python I choose you!";
+  } else if (getSelectedValue == "sweet") {
+    document.getElementById("disp").innerHTML = "Javascript I choose you!";
+  } else if (getSelectedValue == "both") {
+    document.getElementById("disp").innerHTML = "Golang....sure!";
+  }
 }
-result.addEventListener("click", questionOne);
